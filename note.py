97b975fe79065485b5cc40f053b517122778792f
VALERIA <valeri.tereshenkova@icloud.com>
2023-11-05 12:22:13 +0300

    Completed Task

---
+++ note.py
@@ -0,0 +1,7 @@
+class Note:
+    def __init__(self, title,  description):
+        self.title = title
+        self.description = description
+
+    def signal(self):
+        return f"Заметка <{self.title}> создана"
